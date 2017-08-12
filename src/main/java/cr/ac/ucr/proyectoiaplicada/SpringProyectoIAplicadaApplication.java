fa10f628cb45b562dfdc5c19382f642ca7229a0b
jose-coto <gerrall@hotmail.com>
2017-08-11 21:18:18 -0600

    Creacion proyecto Commit

---
+++ src/main/java/cr/ac/ucr/proyectoiaplicada/SpringProyectoIAplicadaApplication.java
@@ -0,0 +1,12 @@
+package cr.ac.ucr.proyectoiaplicada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProyectoIAplicadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProyectoIAplicadaApplication.class, args);
+	}
+}
