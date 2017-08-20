52e4b08b2e1dcfd59e154c4897a17908b524368b
jose-coto <gerrall@hotmail.com>
2017-08-19 19:39:27 -0600

    Arreglo rutas relativas

--- src/main/resources/static/js/IframeSize.js
+++
@@ -1,5 +0,0 @@
-function onResize() {
-    var iframe = window.parent.document.getElementById('iframe');
-    iframe.style.width = window.style.width;
-    iframe.style.height = window.style.height;
-}
