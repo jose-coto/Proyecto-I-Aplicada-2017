8a1ab6f2c22ed4fe02c0dedbe1aabe2f0dbe2bb8
koran <koran@LAPTOP-GNU6K9IA>
2017-08-25 11:07:00 -0600

    Interfaz registro y login

--- src/main/java/cr/ac/ucr/proyectoiaplicada/controller/RegisterController.java
+++ src/main/java/cr/ac/ucr/proyectoiaplicada/controller/RegisterController.java
@@ -1,13 +1,15 @@
 package cr.ac.ucr.proyectoiaplicada.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class RegisterController {
 
 	@RequestMapping(value = {"/Register"}, method = RequestMethod.GET)
 	public String Principal(Model model) {
-		return "Register";
+		return "Login";
 	}
 }
