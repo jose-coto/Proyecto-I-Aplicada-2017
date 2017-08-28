2eadb54497135071b4a99a946235536265688f38
jose-coto <gerrall@hotmail.com>
2017-08-27 23:34:00 -0600

    Internacionalizacion en header y principal, list quedo casi lista pero faltan detalles

---
+++ src/main/java/cr/ac/ucr/proyectoiaplicada/config/Config.java
@@ -0,0 +1,48 @@
+package cr.ac.ucr.proyectoiaplicada.config;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+
+@Configuration
+@ComponentScan(basePackages = "cr.ac.ucr.proyectoiaplicada.config")
+public class Config extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public LocaleResolver localeResolver() {
+	    SessionLocaleResolver slr = new SessionLocaleResolver();
+	    slr.setDefaultLocale(Locale.ROOT);
+	    return slr;
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+	    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+	    lci.setParamName("lang");
+	    return lci;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+	    registry.addInterceptor(localeChangeInterceptor());
+	}
+	
+	/*
+	@Bean(name = "multipartResolver")
+	public CommonsMultipartResolver getCommonsMultipartResolver() {
+	    CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+	    multipartResolver.setMaxUploadSize(20971520);   
+	    multipartResolver.setMaxInMemorySize(1048576);
+	    return multipartResolver;
+	}
+	*/
+}
